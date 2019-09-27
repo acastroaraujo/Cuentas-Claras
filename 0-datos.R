fa45ac7daea4eb39bfd5f5b74799950616f74832
andrés castro araújo <acastroaraujo@gmail.com>
2019-09-27 18:09:13 -0400

    d

--- 0-datos.R
+++ 0-datos.R
@@ -1,7 +1,11 @@
-
+library(tidyverse)
 library(tabulizer)
 
 candidatos <- list.files("datos")
 info <- list.files(paste0("datos/", candidatos[[1]]))
 
-str_detect()
+i <- str_detect(info, "Anexo_5.1B")
+
+f <- file(paste0("datos/", candidatos, "/", info[i]))
+close(f)
+
