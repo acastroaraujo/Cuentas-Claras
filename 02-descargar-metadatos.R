9a75c4c6ef497959f03bd2183858df3e674ebe32
acastroaraujo <acastroaraujo@gmail.com>
2020-12-07 14:25:29 -0500

    update

--- 02-descargar-metadatos.R
+++ 02-descargar-metadatos.R
@@ -1,35 +1,61 @@
 
 source("01-scraper.R")
 
-# Meta Datos --------------------------------------------------------------
+# Meta Datos 
 
-outfolder <- "metadatos-territorial/"
+retrieve_n <- function(outfolder, y) {
+  
+  files <- dir(outfolder)
+  
+  index <- which(str_detect(files, paste0("^", y)))
 
-if (!dir.exists(outfolder)) dir.create(outfolder)
-n <- 1
-
-if (!is_empty(dir(outfolder))) {
- 
-  n <- str_replace(dir(outfolder), "\\d{4}-reporte-(\\d+)\\.rds", "\\1") %>% 
+  out <- files[index]
+  
+  if (is_empty(out)) return(1)
+  
+  out <- out %>% 
+    str_replace("\\d{4}-reporte-(\\d+)\\.rds", "\\1") %>% 
     as.numeric() %>% 
     max()
   
-  n <- n + 1
+  return(out + 1)
+  
 }
+
+# Territorial -------------------------------------------------------------
+
+outfolder <- "metadatos-territorial/"
+y <- "2015"
+
+if (!dir.exists(outfolder)) dir.create(outfolder)
+
+if (!is_empty(dir(outfolder))) {
+ 
+  n <- retrieve_n(outfolder, y)
+  
+} else {
+  
+  n <- 1
+  
+}
+
+errores <- 0
 
 repeat {
   
-  out <- try(cc_buscador_territorial(n, "2019"))
-  filename <- str_glue("{outfolder}2019-reporte-{n}.rds")
+  out <- try(cc_buscador_territorial(n, y))
+  filename <- str_glue("{outfolder}{y}-reporte-{n}.rds")
   write_rds(out, filename, compress = "gz")
   n <- n + 1
   
   errores <- if (any(class(out) == "try-error")) errores + 1 else 0
-  if (errores >= 3) break
+  if (errores >= 15) break
   
   Sys.sleep(runif(1, 1, 3))     ## be kind
   
 }
+
+# Debug
 
 error_index <- dir(outfolder, full.names = TRUE) %>% 
   map(read_rds) %>% 
@@ -37,4 +63,46 @@ error_index <- dir(outfolder, full.names = TRUE) %>%
   which()
 
 length(error_index)
+
+
+# Legislativo -------------------------------------------------------------
+
+outfolder <- "metadatos-legislativo/"
+y <- "2014"
+
+if (!dir.exists(outfolder)) dir.create(outfolder)
+
+if (!is_empty(dir(outfolder))) {
   
+  n <- retrieve_n(outfolder, y)
+  
+} else {
+  
+  n <- 1
+  
+}
+
+errores <- 0
+
+repeat {
+  
+  out <- try(cc_buscador_legislativo(n, y))
+  filename <- str_glue("{outfolder}{y}-reporte-{n}.rds")
+  write_rds(out, filename, compress = "gz")
+  n <- n + 1
+  
+  errores <- if (any(class(out) == "try-error")) errores + 1 else 0
+  if (errores >= 15) break
+  
+  Sys.sleep(runif(1, 1, 3))     ## be kind
+  
+}
+
+# Debug
+
+error_index <- dir(outfolder, full.names = TRUE) %>% 
+  map(read_rds) %>% 
+  map_lgl(~ any(class(.x) == "try-error")) %>% 
+  which()
+
+length(error_index)
